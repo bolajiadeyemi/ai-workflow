c435a7042bd817c0ae35901f66beef0dacd2629b
Bolaji Adeyemi <d.bola1@gmail.com>
2025-07-03 06:49:47 +0100

    refactor workflow to combine multipe steps

--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 Here's a link to something that looks like terraform in AI world [OpenRouter](https://openrouter.ai/)
 
 [uv](https://docs.astral.sh/uv/getting-started/installation/#standalone-installer)
+
+[academind](https://academind.com/courses)
+
+[Maximilian Schwarzmueller](https://maximilian-schwarzmueller.com/articles/gemma-3n-may-be-amazing/)
--- main.py
+++ main.py
@@ -3,8 +3,56 @@ from dotenv import load_dotenv
 load_dotenv()
 
 from openai import OpenAI
-import json 
+import json
+import requests
 client = OpenAI()
+
+
+def get_website_html(url: str) -> str:
+    try:
+        response = requests.get(url)
+        response.raise_for_status() # Raise an error for bad responses
+        return response.text
+    except requests.RequestException as e:
+        print(f"Error fetching the URL {url}: {e}")
+        return ""
+
+
+def extract_core_website_content(html: str) -> str:
+    response = client.responses.create(
+        # using gpt-4o-mini because it's great for summarization & extraction tasks ( and cheap!)
+        model = "gpt-4o-mini",
+        input=f"""
+        You are an expert web content extractor. Your task is to extract the core content from a give HTML page.
+        The core content should be the main text, excluding navigation, footers, and other non-essential elements like scripts etc
+
+        Here is the HTML content:
+        <html>
+        {html}
+        </html>
+
+        Please extract the core content and return it as plain text.
+         """
+    )
+
+    return response.output_text
+
+
+def summarize_content(content: str) -> str:
+    response = client.responses.create(
+            model = "gpt-4o-mini",
+            input=f"""
+             You are an expert summarizer. Your task is to summarize the provided content into a concise and clear summary.
+
+             Here is the content summarize:
+             <content>
+             {content}
+             </content>
+           """
+    )
+
+    return response.output_text
+
 
 def generate_x_post(topic: str) -> str:
     # call AI / LLM
@@ -60,11 +108,37 @@ Don't use the content from the examples!
 def main():
     # I want to develop AI workflow
     # That takes some user input => AI (LLM) to generate X post => output post
-    print("Hello from essentials!")
+    print("Hello from your AI powered workflow")
 
-    user_input = input("What should the post be about? ")
-    x_post = generate_x_post(user_input)
-    print("Generated X post")
+
+    website_url = input("Website URL: ")
+    print("Fetching website HTML....")
+
+    try:
+        html_content = get_website_html(website_url)
+    except Exception as e:
+        print(f"An error occurred while fetching the website: {e}")
+        return
+    
+    if not html_content:
+        print("Failed to fetch the website content. Exiting.")
+
+    print("--------------------")
+    print("Extracting core content from the website...")
+    core_content = extract_core_website_content(html_content)
+    print("Extracted core content:")
+    print(core_content)
+
+    print("-----------------------------")
+    print("Summarizing the core content...")
+    summary = summarize_content(core_content)
+    print("Generated summary:")
+    print(summary)
+
+    print("-----------------------------")
+    print("Generating X post based on the summary...")
+    x_post = generate_x_post(summary)
+    print("Generated X post:")
     print(x_post)
 
 if __name__ == "__main__":
