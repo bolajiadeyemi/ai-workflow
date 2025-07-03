eb408e78c00a159c54f99ab2cb5d38f5ae27a419
Bolaji Adeyemi <d.bola1@gmail.com>
2025-07-03 05:08:21 +0100

    init commit

---
+++ README.md
@@ -0,0 +1,5 @@
+
+
+Here's a link to something that looks like terraform in AI world [OpenRouter](https://openrouter.ai/)
+
+[uv](https://docs.astral.sh/uv/getting-started/installation/#standalone-installer)
---
+++ main.py
@@ -0,0 +1,57 @@
+import os
+
+import requests
+from dotenv import load_dotenv
+
+load_dotenv()
+
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+
+def generate_x_post(topic: str) -> str:
+    # call AI / LLM
+    # pass
+
+    prompt = f"""
+        You are an expert social media manager, and you excel at crafting viral and highly engaging posts for X (formerly Twitter).
+
+        Your task is to generate a post that is concise, impactful, and tailored to the topic provided by the user
+        Avoid using hashtags and lots of emojis (a few emojis are okay, but not too many).
+
+        Keep the post short and focused, structure it in a clean, readable way, using line breaks and empty lines to enhance readability.
+
+        Here's the topic provided by the user for which you need to generate a post:
+
+        <topic>
+        {topic}
+        </topic>
+"""
+
+    payload = {
+        "model": "gpt-4o",
+        "input": prompt
+    }
+    response = requests.post(
+        "https://api.openai.com/v1/responses",
+        json=payload,
+        headers={
+            "Content-Type": "application/json",
+            "Authorization": f"Bearer {OPENAI_API_KEY}"
+        }
+        )
+    
+    response_text = response.json().get("output", [{}])[0].get("content", [{}])[0].get("text", "")
+
+    return response_text
+
+def main():
+    # I want to develop AI workflow
+    # That takes some user input => AI (LLM) to generate X post => output post
+    print("Hello from essentials!")
+
+    user_input = input("What should the post be about? ")
+    x_post = generate_x_post(user_input)
+    print("Generated X post")
+    print(x_post)
+
+if __name__ == "__main__":
+    main()
